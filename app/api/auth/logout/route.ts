42eb7198d80979a63c17d01533c3e3448a2f137e
LDukeH <24520378@gm.uit.edu.vn>
2025-08-03 23:44:40 +0700

    logout token fix 2

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,13 +1,10 @@
 // app/api/logout/route.ts
 import { NextResponse } from "next/server";
 
-import { cookies } from "next/headers";
-
 export async function POST() {
   const res = NextResponse.json({ message: "Logged out" });
 
-  const cookieStore = await cookies();
-  cookieStore.set("token", "", {
+  res.cookies.set("token", "", {
     path: "/",
     maxAge: 0,
     httpOnly: true,
